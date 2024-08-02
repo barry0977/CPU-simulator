7a22df9894b8a40d78544f26955294bfb836a7fc
Barry <2411557145@qq.com>
2024-08-02 22:21:11 +0800

    乱序

--- cpu.h
+++ cpu.h
@@ -74,8 +74,8 @@ public:
 //        IQ.execute(&RoB,&RS,&LSB,&RF,&cdb,&pre);
         IQ.execute(&RoB,&RS,&LSB,&RF,&cdb,&pre);
         int res=RoB.commit(&RF,mem,&cdb,&pre);
-        RS.execute(&alu,&RoB,&cdb);
         LSB.execute(mem,&RoB,&cdb);
+        RS.execute(&alu,&RoB,&cdb);
         if(res>=0){//如果发现预测错误
             flush(res);
         }
